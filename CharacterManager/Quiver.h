8c1b53fdc8304c5cbee2811dfb2524867be74073
Robert <rcmayhewii@gmail.com>
2018-03-05 02:25:12 -0800

    started the first step of map vs set for the quiver

--- CharacterManager/Quiver.h
+++ CharacterManager/Quiver.h
@@ -1,9 +1,19 @@
 #pragma once
 #include "Stuff.h"
+#include "Ammo.h"
 #include <algorithm>
+#include <vector>
 class Quiver :
 	public Stuff
 {
+private:
+	//need to make this a hash table?
+	//or is it small enough that I only need an ordered list?
+	//or binary serach tree?
+
+	//the collection only stores the types of ammo, the uses will be the number of
+	//that ammo type in the quiver 
+	std::vector <Ammo> collection;
 public:
 	Quiver();
 	virtual ~Quiver();
