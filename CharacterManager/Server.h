9f46c04582859863b066f0c7a0c2df2ea6cbca45
rcmayhew <rcmayhewii@gmail.com>
2018-03-05 17:24:22 -0800

    changed dependencies to be files

--- CharacterManager/Server.h
+++ CharacterManager/Server.h
@@ -1,4 +1,15 @@
 #pragma once
+/*
+Include directly the different
+headers from cppconn/ and mysql_driver.h + mysql_util.h
+(and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include "cppconn/driver.h"
+#include "cppconn/exception.h"
+#include "cppconn/resultset.h"
+#include "cppconn/statement.h"
 class Server
 {
 public:
