3c747fbc1d6404f177c76a690b6c3fc277e5a239
rcmayhew <rcmayhewii@gmail.com>
2018-05-02 13:16:25 -0700

    added private caontainer to Usable

--- CharacterManager/Usable.h
+++ CharacterManager/Usable.h
@@ -1,8 +1,20 @@
 #pragma once
 #include "Stuff.h"
+#include "Multi.h"
+#include <memory>
+#include <unordered_map>
+
+// /typedefs for easier readiblity
+// Element is for a weapon type and the interface class
+typedef std::shared_ptr<Item> Element;
+// unordered_map that uses the Element name for the key
+typedef std::unordered_map<std::string, Element> Backpack;
+
 class Usable :
 	public Stuff
 {
+private:
+	Backpack backpack;
 public:
 	Usable();
 	virtual ~Usable();
