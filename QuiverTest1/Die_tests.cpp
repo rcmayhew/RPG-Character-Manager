0aaec44483f1f5a2a289b1b0a625e0c79d966000
rcmayhew <rcmayhewii@gmail.com>
2018-05-30 14:19:05 -0700

    added die tests

---
+++ QuiverTest1/Die_tests.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../CharacterManager/Quiver.h"
+// #include "../CharacterManager/Quiver.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(Die_Tests)
+	{
+	public:
+
+		
+	};
+}
